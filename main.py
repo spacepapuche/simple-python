a19601a8ea6d0d8f117715fce5e66c570ab05831
BadouPapuche <52207725+BadouPapuche@users.noreply.github.com>
2020-12-20 22:08:03 +0100

    MAJ 2

--- main.py
+++ main.py
@@ -5,3 +5,15 @@ def afficher(n):
 #Commande additionner
 def additionner(n1,n2):
   print(n1+n2)
+
+#Commande soustraire
+def soustraire(n1,n2):
+  print(n1-n2)
+
+#Commande multiplier
+def multiplier(n1,n2):
+  print(n1*n2)
+
+#Commande diviser
+def diviser(n1,n2):
+  print(n1/n2)
