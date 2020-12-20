8aa1e08fd2509a16fa0aa0773586ac4ce62bd451
BadouPapuche <52207725+BadouPapuche@users.noreply.github.com>
2020-12-20 22:47:16 +0100

    Maj répéter

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+print("Lancement de simple-python en cours, veuillez patienter...")
+
 #Commande afficher
 def afficher(n):
   print(n)
@@ -17,3 +19,18 @@ def multiplier(n1,n2):
 #Commande diviser
 def diviser(n1,n2):
   print(n1/n2)
+
+#Commande répéter 1
+def répéter(n, ndeux):
+  for n in range(n):
+    print(ndeux)
+
+#Commande répéter 2
+def répéterdeux(n, a, ntrois):
+  for n in range(n, a):
+    print(ntrois)
+
+#Commande répéter 3
+def répétertrois(n, a, b, nquatre):
+  for n in range(n,a, b):
+    print(nquatre)    
